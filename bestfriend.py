5240511c1e75c6af04a027a194a150c85c52f0c3
Austin Hancock <austin.hancock075@gmail.com>
2022-12-05 13:31:58 -0500

    Loops until right

--- bestfriend.py
+++ bestfriend.py
@@ -2,3 +2,13 @@ first_name = input("what is your name? ")
 print(f"Hello {first_name}",)
 best = input("Who is the best? ")
 print(f"NO! {best} is not the best, I am the best!")
+while True:
+
+    if best == first_name:
+        print("You Are thes best as well!!!")
+        break
+    else:
+        first_name = input("what is your name? ")
+        print(f"Hello {first_name}",)
+        best = input("Who is the best? ")
+        print(f"NO! {best} is not the best, I am the best!")
