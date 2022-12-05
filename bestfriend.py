f8491fb6c7b77daca98f3ee74eac0251f2a96cc0
Austin Hancock <austin.hancock075@gmail.com>
2022-12-05 13:49:52 -0500

    Another weird commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,14 +1,10 @@
-first_name = input("what is your name? ")
-print(f"Hello {first_name}",)
-best = input("Who is the best? ")
-print(f"NO! {best} is not the best, I am the best!")
-while True:
+first_name = 0
+best = 1
 
-    if best == first_name:
-        print("You Are thes best as well!!!")
-        break
-    else:
-        first_name = input("what is your name? ")
-        print(f"Hello {first_name}",)
-        best = input("Who is the best? ")
-        print(f"NO! {best} is not the best, I am the best!")
+while best != first_name:
+    first_name = input("what is your name? ")
+    print(f"Hello {first_name}",)
+    best = input("Who is the best? ")
+    print(f"NO! {best} is not the best, I am the best!")
+
+print("You Are thes best as well!!!")
